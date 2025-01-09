9cab918f7c96ffed7b1ee870512bf0455098b041
Jerie See Villanueva <codestackjs007@gmail.com>
2025-01-08 21:00:56 -0800

    spinner

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+export const createIssueSchema = z.object({
+    title: z.string().min(1,"Title is required").max(255),
+    description: z.string().min(1)
+});
+// import React, { ReactNode } from 'react'
+
+// interface Props {
+//     children: ReactNode;
+// }
+
+// const ErrorMessage = ({children}:Props) => {
+//   return (
+//     <div>ErrorMessage</div>
+//   )
+// }
+
+// export default ErrorMessage
+
+import { Text } from '@radix-ui/themes'
+import React, { PropsWithChildren } from 'react'
+
+const ErrorMessage = ({children}:PropsWithChildren) => {
+
+    if(!children) return null
+
+  return (
+    <>
+        <Text color='red' as='p'>{children}</Text>
+    </>
+  )
+}
+
+export default ErrorMessage
